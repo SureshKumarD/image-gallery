316877e5e2b53c7d949d5384c76716bd40c8ab8e
suresh.durairaj <suresh.durairaj@via.com>
2016-04-05 22:13:30 +0530

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Suresh on 4/5/16.
+//  Copyright © 2016 Suresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
